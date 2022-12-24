fa72790f00d29d23e6ff690e4b9d141e3b05b43e
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2022-12-24 11:24:25 +0100

    Grid system

---
+++ TurnBasedStrategyGame/Assets/Scripts/GridObject.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject
+{
+
+    private GridSystem gridSystem;
+    private GridPosition gridPosition;
+    private List<Unit> unitList;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem;
+        this.gridPosition = gridPosition;
+        unitList = new List<Unit>();
+    }
+
+    public void AddUnit(Unit unit)
+    {
+        unitList.Add(unit);
+    }
+
+    public void RemoveUnit(Unit unit)
+    {
+        unitList.Remove(unit);
+    }
+
+    public List<Unit> GetUnitList()
+    {
+        return unitList;
+    }
+
+}
---
+++ TurnBasedStrategyGame/Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGrid : MonoBehaviour
+{
+
+    public static LevelGrid Instance { get; private set; }
+
+    private GridSystem gridSystem;
+
+    private void Awake()
+    {
+        Instance = this;
+        gridSystem = new GridSystem(10, 10, 2f);
+    }
+
+    public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
+    {
+        gridSystem.GetGridObject(gridPosition).AddUnit(unit);
+    }
+
+    public List<Unit> GetUnitListAtGridPosition(GridPosition gridPosition)
+    {
+        return gridSystem.GetGridObject(gridPosition).GetUnitList();
+    }
+
+    public void RemoveUnitAtGridPosition(GridPosition gridPosition, Unit unit)
+    {
+        gridSystem.GetGridObject(gridPosition).RemoveUnit(unit);
+    }
+
+    public void UnitMovedGridPosition(Unit unit, GridPosition fromGridPosition, GridPosition toGridPosition)
+    {
+        RemoveUnitAtGridPosition(fromGridPosition, unit);
+        AddUnitAtGridPosition(toGridPosition, unit);
+    }
+
+    public GridPosition GetGridPosition(Vector3 worldPosition) => gridSystem.GetGridPosition(worldPosition);
+
+}
