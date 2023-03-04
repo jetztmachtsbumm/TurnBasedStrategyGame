018e78bb1514a3fb90c904d6db537d8b4640ed10
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2023-03-04 14:52:18 +0100

    Generic actions on unit

--- TurnBasedStrategyGame/Assets/Scripts/Unit.cs
+++ TurnBasedStrategyGame/Assets/Scripts/Unit.cs
@@ -15,18 +15,12 @@ public class Unit : MonoBehaviour
 
     private GridPosition gridPosition;
     private HealthSystem healthSystem;
-    private MoveAction moveAction;
-    private SpinAction spinAction;
-    private ShootAction shootAction;
     private BaseAction[] baseActionArray;
     private int actionPoints = ACTION_POINTS_MAX;
 
     private void Awake()
     {
         healthSystem = GetComponent<HealthSystem>();
-        moveAction = GetComponent<MoveAction>();
-        spinAction = GetComponent<SpinAction>();
-        shootAction = GetComponent<ShootAction>();
         baseActionArray = GetComponents<BaseAction>();
     }
 
@@ -76,19 +70,17 @@ public class Unit : MonoBehaviour
         return healthSystem.GetHealthNormalized();
     }
 
-    public MoveAction GetMoveAction()
+    public T GetAction<T>() where T : BaseAction
     {
-        return moveAction;
-    }
+        foreach(BaseAction baseAction in baseActionArray)
+        {
+            if(baseAction is T)
+            {
+                return (T)baseAction;
+            }
+        }
 
-    public SpinAction GetSpinAction()
-    {
-        return spinAction;
-    }
-
-    public ShootAction GetShootAction()
-    {
-        return shootAction;
+        return null;
     }
 
     public GridPosition GetGridPosition()
